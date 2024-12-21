ae67616eaa4b0688053607e51881a1c6f3b8a49b
Carla Martínez Alonso <carlalonsx@gmail.com>
2024-12-21 13:39:02 +0100

    commit1

--- app/src/main/java/com/example/recipesapp/MainActivity.java
+++ app/src/main/java/com/example/recipesapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.recipesapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.recipesapp.activities.EntrantesActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        @SuppressLint("MissingInflatedId")
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+
+            Button btn_entrante = findViewById(R.id.btn_entrante);
+            Button btn_principal = findViewById(R.id.btn_principal);
+            Button btn_segundos = findViewById(R.id.btn_segundos);
+            Button btn_postres = findViewById(R.id.btn_postres);
+            Button btn_reposteria = findViewById(R.id.btn_reposteria);
+            Button btn_panaderia = findViewById(R.id.btn_panadería);
+
+            // Al hacer clic en el botón 'Entrantes'
+            btn_entrante.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, EntrantesActivity.class);
+                startActivity(intent); // Aquí se inicia la actividad
+            });
+        }
     }
-}
+
